520be15762c4538d6e2980cb587732bd7bc1531c
Michael Mitrakos <mitrakmt@Michaels-MBP.fios-router.home>
2018-05-13 16:49:55 -0700

    feat: docs are live! :party:

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(helmet())
 
-app.use("/docs", express.static(__dirname + '/docs/index.html'));
+app.use("/docs", express.static(__dirname + '/docs'));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
