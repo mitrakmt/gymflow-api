ee7c3091f96f03e19b060cb2abd8765b01b04bd6
Michael Mitrakos <mitrakmt@Michaels-MacBook-Pro.local>
2018-05-16 00:28:51 -0500

    feat: mailgun integration

--- controllers/email.js
+++ controllers/email.js
@@ -1,7 +1,7 @@
 let emailController = {}
 let Mailchimp = require('mailchimp-api-v3')
 let mailchimp = new Mailchimp(process.env.MAILCHIMP_API);
-var mailgun = require('mailgun-js')({ apiKey: process.env.MAILGUN_API, domain: process.env.MAILGUN_DOMAIN });
+let mailgun = require('mailgun-js')({ apiKey: process.env.MAILGUN_API, domain: process.env.MAILGUN_DOMAIN });
 
 emailController.CONTACT_US = (req, res) => {
   let message = req.body.message
@@ -12,34 +12,35 @@ emailController.CONTACT_US = (req, res) => {
   let emailData = {
     from: email,
     to: 'contact@gymflow.app',
-    subject: "[URGENT] - Topic: " + topic,
+    subject: "[CONTACT] - Topic: " + topic,
     text: "Name: " + name + ", Topic: " + topic + "\n \n" + message
   }
-  console.log('emial', emailData)
 
-  mailgun.messages().send(emailData, (error, body) => {
-    if (error) {
+  mailgun.messages().send(emailData, (err, body) => {
+    if (err) {
       res.status(400).send({
         sent: false,
-        error
+        error: err
       })
-    } else {
+    }
+    else {
       res.status(200).send({
         sent: true
       })
     }
-  })
+  });
 }
 
 emailController.SIGN_UP = (req, res) => {
   let email = req.body.email
-  let first_name = req.body.firstName
-  let last_name = req.body.lastName
+  let firstName = req.body.firstName
+  let lastName = req.body.lastName
+  // TODO: names not being saved correctly
 
-  mailchimp.post('/lists/7fe6db1a0d/members', {
+  mailchimp.post('/lists/627367da02/members', {
     email_address: email,
-    first_name,
-    last_name,
+    firstName,
+    lastName,
     status: 'subscribed'
   })
     .then(results => {
