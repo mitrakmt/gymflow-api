759ea699d0d833f1deeb66216cdae914f1091527
Michael Mitrakos <mitrakmt@Michaels-MacBook-Pro.local>
2018-05-16 22:54:03 -0500

    feat: update docs

--- models/email.js
+++ models/email.js
@@ -1,3 +1,18 @@
 let emailModel = {}
+let Users = require('../db').Users
+
+emailModel.PASSWORD_RESET = (email, token) => {
+    return Users.update({
+        token
+      }, {
+        where: { 
+            email
+        }
+      })
+      .then(result => {
+        return `https://www.gymflow.app/passwordreset/${token}`
+      });
+}
+
 
 module.exports = emailModel
