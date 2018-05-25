194b1df790a6843130c433e3953f5bd801c91a77
Michael Mitrakos <mitrakmt@Michaels-MBP.fios-router.home>
2018-05-25 12:36:26 -0700

    fix: update id checks within workouts

--- middleware/validateWorkoutSchema.js
+++ middleware/validateWorkoutSchema.js
@@ -29,7 +29,7 @@ let validateWorkoutSchema = (req, res, next) => {
         }
     
         // Check for set ID and type of number
-        if (!workout[setIndex].id || typeof workout[setIndex].id !== 'number') {
+        if (workout[setIndex].id === undefined || typeof workout[setIndex].id !== 'number') {
             res.status(400).send({
                 error: "Each set must contain an ID that is of type number"
             })
@@ -47,7 +47,7 @@ let validateWorkoutSchema = (req, res, next) => {
         // Loop through and check exercise schema
         for (let exerciseIndex = 0; exerciseIndex < workout[setIndex].exercises.length; exerciseIndex++) {
             // Check for IDs and type of number
-            if (!workout[setIndex].exercises[exerciseIndex].id || typeof workout[setIndex].exercises[exerciseIndex].id !== 'number') {
+            if (workout[setIndex].exercises[exerciseIndex].id === undefined || typeof workout[setIndex].exercises[exerciseIndex].id !== 'number') {
                 res.status(400).send({
                     error: "Each excersize must contain an ID that is of type number"
                 })
